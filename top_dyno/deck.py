b2456b33bcee09fa79b115c246e52cce840d2e64
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-04 13:59:25 -0300

    refactor :hammer: refactored generate dyno data  function to random numbers

--- top_dyno/deck.py
+++ top_dyno/deck.py
@@ -7,7 +7,7 @@ def generate_random_dynos_data() -> list[dict]:
     dyno_cards = []
 
     for dyno_name in DYNO_NAMES:
-        dyno_dict  = {'name': dyno_name, 'strength': 10, 'agility': 8, 'heigth': 7}
+        dyno_dict  = {'name': dyno_name, 'strength': random.randint(1, 10), 'agility': round(random.uniform(0, 10), 1), 'heigth': round(random.uniform(0, 10), 2)}
         dyno_cards.append(dyno_dict)
 
     return dyno_cards
