e5e13261ee66a0b09268bf1f9c7b200c485dfc8b
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-04 13:36:04 -0300

    feat :sparkles: created basic folders structures and json_handler, deck functions

---
+++ utils/json_handler.py
@@ -0,0 +1,12 @@
+import json
+
+
+import ujson as json
+
+def read_json(filepath: str) -> list[dict]:
+    with open(filepath, 'r') as file:
+        return json.load(file)
+
+def write_json(filepath: str, payload: list[dict]) -> None:
+    with open(filepath, 'w') as file:
+        json.dump(payload, file, indent=2)
