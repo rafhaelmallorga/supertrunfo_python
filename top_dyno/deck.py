64e8e93e6e11b2e396b7053bb0030b13dacb4960
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-04 13:49:18 -0300

    feat :sparkles: created generate randon dynos data function

--- top_dyno/deck.py
+++ top_dyno/deck.py
@@ -1,1 +1,12 @@
 from utils import read_json, write_json
+
+DYNO_NAMES = ("Mallorgassauro Rex", "Devceratops", "Pythondonte", "Galinha Caipira")
+
+def generate_random_dynos_data() -> list[dict]:
+    dyno_cards = []
+
+    for dyno_name in DYNO_NAMES:
+        dyno_dict  = {'name': dyno_name, 'strength': 10, 'agility': 8, 'heigth': 7}
+        dyno_cards.append(dyno_dict)
+
+    return dyno_dict
